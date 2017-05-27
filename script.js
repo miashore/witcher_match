a926dfbcfd8cb3734bf05d409ecf29217d6135ef
miashore <mcbashore@gmail.com>
2017-05-26 19:07:48 -0700

    removed particles and added media query for portrait mode that needs more work

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ var gameplayCount = 1;
 
 function initializeGame(){
     attachClickHandlers();
-    particles();
     shuffleCards();
 }
 function attachClickHandlers(){
@@ -145,119 +144,6 @@ function rulesClickHandler(){
     $('.rules').click(function(){
         $('#rules-modal').modal('show');
     });
-}
-function particles(){
-    var PARTICLE_QUANT = 60;
-    var FPS = 60;
-    var BOUNCE = -1;
-    var PARTICLE_COLOR = 'rgba(200, 0, 50,.6)';
-    var ARC_RADIUS = 1;
-    var Particles = function($element) {
-        if ($element.length === 0) { return; }
-        this.$element = $element;
-        this.lastTimeStamp = null;
-        this.particles = [];
-        this.init();
-    };
-    var proto = Particles.prototype;
-    proto.init = function() {
-        this.createChildren();
-        this.layout();
-        this.enable();
-    };
-    proto.createChildren = function() {
-        this.canvas = this.$element[0];
-        this.context = this.canvas.getContext('2d');
-        this.canvasWidth = this.canvas.width;
-        this.canvasHeight = this.canvas.height;
-        this.lastTimeStamp = new Date().getTime();
-        return this;
-    };
-    proto.layout = function() {
-        window.requestAnimFrame = (function() {
-            return  window.requestAnimationFrame       ||
-                window.webkitRequestAnimationFrame         ||
-                window.mozRequestAnimationFrame;
-        })();
-        return this;
-    };
-    proto.removeChildren = function() {
-        this.context = null;
-        this.canvasWidth = null;
-        this.canvasHeight = null;
-        this.lastTimeStamp = null;
-        return this;
-    };
-    proto.enable = function() {
-        this.createParticleData();
-        this.renderLoop();
-    };
-    proto.createParticleData = function() {
-        var i = 0;
-        var l = PARTICLE_QUANT;
-
-        for(; i < l; i++) {
-            this.particles[i] = {};
-            this.setParticleData(this.particles[i]);
-        }
-    };
-    proto.setParticleData = function(particle) {
-        particle.x = Math.random() * this.canvasWidth;
-        particle.y = Math.random() * this.canvasHeight;
-        particle.vx = (Math.random()) - 0.5;
-        particle.vy = (Math.random()) - 0.5;
-    };
-    proto.update = function() {
-        var i = 0;
-        var l = PARTICLE_QUANT;
-
-        for (; i < l; i++) {
-            var particle = this.particles[i];
-
-            particle.x += particle.vx;
-            particle.y += particle.vy;
-
-            if (particle.x > this.canvasWidth) {
-                particle.x = this.canvasWidth;
-                particle.vx *= BOUNCE;
-            } else if (particle.x < 0) {
-                particle.x = 0;
-                particle.vx *= BOUNCE;
-            }
-
-            if (particle.y > this.canvasHeight) {
-                particle.y = this.canvasHeight;
-                particle.vy *= BOUNCE;
-            } else if (particle.y < 0) {
-                particle.y = 0;
-                particle.vy *= BOUNCE;
-            }
-        }
-    };
-    proto.draw = function() {
-        var i = 0;
-
-        if (!this.context) {
-            return;
-        }
-        this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-        this.context.strokeStyle = PARTICLE_COLOR;
-
-        for(; i < PARTICLE_QUANT; i++) {
-            var particle = this.particles[i];
-            this.context.save();
-            this.context.beginPath();
-            this.context.arc(particle.x, particle.y, ARC_RADIUS, 0, Math.PI * 2);
-            this.context.stroke();
-            this.context.restore();
-        }
-    };
-    proto.renderLoop = function() {
-        requestAnimationFrame(this.renderLoop.bind(this));
-        this.update();
-        this.draw();
-    };
-    var particles = new Particles($('#js-particles'));
 }
 function resetGame(){
     if(trueMatch === totalPossibleMatches){
